4f3a4feb36d1761e22444c89bcb19c3074c2eb0f
Sara Margarido <saramargarido@gmail.com>
2024-09-17 18:37:35 +0200

    manipulating the form

---
+++ src/modules/form/submit.js
@@ -0,0 +1,36 @@
+import dayjs from "dayjs"
+
+const form = document.querySelector("form")
+const clientName = document.getElementById("client")
+const selectedDate = document.getElementById("date")
+
+const inputToday = dayjs(new Date()).format("YYYY-MM-DD")
+selectedDate.value = inputToday
+selectedDate.min = inputToday
+
+form.onsubmit = (event) => {
+  event.preventDefault()
+
+  try {
+    const name = clientName.value.trim()
+    if (!name) {
+      return alert("Informe o nome do cliente!")
+    }
+
+    const hourSelected = document.querySelector(".hour-selected")
+    if (!hourSelected) {
+      return alert("Selecione a hora")
+    }
+    const [hour] = hourSelected.innerText.split(":")
+
+    const when = dayjs(selectedDate.value).add(hour, "hour")
+
+    const id = new Date().getTime()
+
+    console.log(id, name, when)
+
+  } catch (error) {
+    alert("Não foi possível realizar o agendamento")
+    console.log(error)
+  }
+}
---
+++ src/modules/page-load.js
@@ -0,0 +1,5 @@
+import {schedulesDay} from "./scheludes/load.js"
+
+document.addEventListener("DOMContentLoaded", function() {
+  schedulesDay()
+})
